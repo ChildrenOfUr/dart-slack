111cfa88722843b3b58a72b818e3b1a71b7c2f7e
darkkiero <paul.vankeuren@gmail.com>
2014-05-29 19:59:56 -0500

    added some informational comments

--- test/test_io.dart
+++ test/test_io.dart
@@ -1,12 +1,20 @@
 import 'package:slack/slack_io.dart';
 
 main() {
- token = 'oaV4CYX5OLlfwO3gciyhBxB8';
- team = 'cou';
+  // The token generated when you set up a new 'incoming webhook' with slack.
+  token = 'your_token';
   
-  Message m = new Message()
-    ..username = 'Street Spirit'
-    ..text = 'I am a spirit!'
-    ..icon_url = 'http://childrenofur.com/assets/street_spirit.png';
-  print(send(m));
+  // The 'team' variable is the first part of your team's slack url,
+  // for example 'cou' would be the team name for 'cou.slack.com/'
+  team = 'your_team';
+   
+  // The various variables within the Message object correspond to what's described in the api.
+  // This information is stored at 'https://api.slack.com/docs/messages' 
+   Message m = new Message()
+     ..username = 'Street Spirit'
+     ..text = 'I am a spirit!'
+     ..icon_url = 'http://childrenofur.com/assets/street_spirit.png';
+   
+   send(m);
+   print(m);
 }
