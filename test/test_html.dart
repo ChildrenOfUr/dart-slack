3691c4085cd65a52a1e5e0d83b688a49c5c6bd25
darkkiero <paul.vankeuren@gmail.com>
2014-07-01 12:32:50 -0500

    removed expired keys and made examples easier to read.

--- test/test_html.dart
+++ test/test_html.dart
@@ -5,15 +5,13 @@ main() {
  token = 'your_token';
  
  // The 'team' variable is the first part of your team's slack url,
- // for example 'cou' would be the team name for 'cou.slack.com/'
  team = 'your_team';
   
  // The various variables within the Message object correspond to what's described in the api.
  // This information is stored at 'https://api.slack.com/docs/messages' 
   Message m = new Message()
-    ..username = 'Street Spirit'
-    ..text = 'I am a spirit!'
-    ..icon_url = 'http://childrenofur.com/assets/street_spirit.png';
+    ..username = 'Professor Botz'
+    ..text = 'I am Professor Botz!';;
   
   send(m);
   print(m);
