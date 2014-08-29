fc808751acddea6b1bd496eb036c880b313fbe11
Robert McDermot <robert.mcdermot@gmail.com>
2014-08-29 09:45:59 -0400

    fixed the test scripts to match the newer API

--- test/test_io.dart
+++ test/test_io.dart
@@ -1,18 +1,18 @@
-
 import 'package:slack/slack_io.dart';
 
-main() {
- token = 'your_token';
- 
- // The 'team' variable is the first part of your team's slack url,
- team = 'your_team';
-  
- // The various variables within the Message object correspond to what's described in the api.
- // This information is stored at 'https://api.slack.com/docs/messages' 
-  Message m = new Message()
-    ..username = 'Professor Botz'
-    ..text = 'I am Professor Botz!';;
-  
-  send(m);
-  print(m);
+main()
+{
+	token = 'your_token';
+
+	// The 'team' variable is the first part of your team's slack url,
+	team = 'your_team';
+
+	// The various variables within the Message object correspond to what's described in the api.
+	// This information is stored at 'https://api.slack.com/docs/messages'
+	String username = 'Professor Botz';
+	String text = 'I am Professor Botz!';
+	Message m = new Message(text, username:username);
+
+	send(m);
+	print(m);
 }
